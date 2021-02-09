1deaf4fcdc0c2761a33ecc58dcf84c88699e263b
duxinyu <8653922+taco-du@user.noreply.gitee.com>
2021-02-09 21:53:16 +0800

    哈希桶的实现

--- HashBucket.hpp
+++ HashBucket.hpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <vector>
 
+#include "common.hpp"
+
 using namespace std;
 
 //哈希桶中管理的是一个个的列表
@@ -17,14 +19,88 @@ struct HashBucketNode
 	T data;
 };
 
-template <class T>
-class HashBucket
+template <class T, class DToI = DToIntDef<T>>
+class HashBucket;
+
+template <class T, class DToI = DToIntDef<T>>
+struct HashBucketIterator
 {
 	typedef HashBucketNode<T> Node;
+	typedef HashBucketIterator<T, DToI> Self;
+public:
+	HashBucketIterator(Node* node = nullptr, HashBucket<T,DToI>* ht = nullptr)
+		:_node(node)
+		,_ht(ht)
+	{}
 
+	T& operator* ()
+	{
+		return _node->data;
+	}
+
+	T* operator->()
+	{
+		return *(operator*());
+	}
+
+	Self& operator++()
+	{
+		Next();
+		return *this;
+	}
+
+	Self operator++(int)
+	{
+		Self temp(*this);
+		Next();
+		return temp;
+	}
+
+	bool operator==(const Self& s)const
+	{
+		return _node == s._node;
+	}
+
+	bool operator!=(const Self& s)const
+	{
+		return _node != s._node;
+	}
+
+private:
+	void Next()
+	{
+		if (_node->next)
+			_node = _node->next;
+		else
+		{
+			//_node为链表的最后一个节点
+			//寻找下一个非空桶
+			//需要知道_node所在的桶号
+			size_t bucketNo = _ht->HashFunc(_ht->BucketCount(), _node->data) + 1;
+			for (; bucketNo < _ht->BucketCount(); bucketNo++)
+			{
+				_node = _ht->_table[bucketNo];
+				if (_node)
+					return;
+			}
+			_node = nullptr;
+		}
+	}
+private:
+	Node* _node;
+	HashBucket<T, DToI>* _ht;
+};
+
+template <class T, class DToI>
+class HashBucket
+{
+	friend HashBucketIterator<T, DToI>;
+	typedef HashBucketNode<T> Node;
+public:
+	typedef HashBucketIterator<T, DToI> iterator;
 public:
 	HashBucket(size_t capacity = 10)
-		:_table(capacity)
+		:_table(GetNextPrime(capacity))
 		,_size(0)
 	{}
 	
@@ -33,10 +109,28 @@ public:
 		Clear();
 	}
 
+	iterator begin()
+	{
+		//第一个非空桶的首节点的位置
+		for (size_t bucketNo = 0; bucketNo < BucketCount(); bucketNo++)
+		{
+			if (_table[bucketNo])
+				return iterator(_table[bucketNo], this);
+		}
+		return end();
+	}
+
+	iterator end()
+	{
+		return iterator(nullptr, this);
+	}
+
 	bool InsertUnique(const T& data)
 	{
+		CheckCapacity();
+
 		//通过哈希函数计算元素所在桶号
-		size_t bucketNo = HashFunc(data);
+		size_t bucketNo = HashFunc(_table.capacity(), data);
 
 		//检测data是否在bucket桶中存在
 		Node* cur = _table[bucketNo];
@@ -59,8 +153,10 @@ public:
 
 	bool InsertEqual(const T& data)
 	{
+		CheckCapacity();
+
 		//通过哈希函数计算元素所在桶号
-		size_t bucketNo = HashFunc(data);
+		size_t bucketNo = HashFunc(_table.capacity(), data);
 		Node* cur = _table[bucketNo];
 
 		//插入新节点--头插
@@ -75,7 +171,7 @@ public:
 	Node* Find(const T& data)
 	{
 		//通过哈希函数计算元素所在桶号
-		size_t bucketNo = HashFunc(data);
+		size_t bucketNo = HashFunc(_table.capacity(), data);
 
 		//查找data在bucket桶中的位置
 		Node* cur = _table[bucketNo];
@@ -94,7 +190,7 @@ public:
 	size_t Erase(const T& data)
 	{
 		//通过哈希函数计算元素所在桶号
-		size_t bucketNo = HashFunc(data);
+		size_t bucketNo = HashFunc(_table.capacity(), data);
 
 		Node* cur = _table[bucketNo];
 		Node* prev = nullptr;	//保存cur的前一个节点
@@ -158,6 +254,17 @@ public:
 		_size = 0;
 	}
 
+	void Swap(HashBucket<T,DToI>& newHt)
+	{
+		_table.swap(newHt._table);
+		swap(_size, newHt._size);
+	}
+
+	size_t BucketCount()const
+	{
+		return _table.capacity();
+	}
+
 	void Print()
 	{
 		for (size_t bucketNo = 0; bucketNo < _table.capacity(); bucketNo++)
@@ -175,22 +282,98 @@ public:
 	}
 
 private:
-	size_t HashFunc(const T& data)
+	void CheckCapacity()
 	{
-		return data % _table.capacity();
+		//这个扩容方法不是很好，在扩容的时候节点存储了俩份
+		/*
+		if (_size == _table.capacity())
+		{
+			//将桶的个数翻倍
+			HashBucket<T,DToI> newHt(_table.capacity() * 2);
+
+			//将旧哈希桶的元素插入新的桶中
+			for (size_t i = 0; i < _table.capacity(); i++)
+			{
+				Node* cur = _table[i];
+				while (cur)
+				{
+					newHt.InsertEqual(cur->data);
+					cur = cur->next;
+				}
+			}
+			this->Swap(newHt);
+		}*/
+
+		//好的扩容方法：把旧桶中的节点搬移到新桶中
+		if (_size == _table.capacity())
+		{
+			vector<Node*> newTable(GetNextPrime(_table.capacity() * 2));
+			for (size_t i = 0; i < _table.capacity(); i++)
+			{
+				Node* cur = _table[i];
+				while (cur)
+				{
+					//将旧桶的节点拆下来
+					_table[i] = cur->next;
+
+					//插入到新桶中
+					size_t bucketNo = HashFunc(newTable.capacity(), cur->data);
+					cur->next = newTable[bucketNo];
+					newTable[bucketNo] = cur;
+
+					//取下一个节点
+					cur = _table[i];
+				}
+			}
+			_table.swap(newTable);
+		}
+	}
+
+	size_t HashFunc(size_t capacity, const T& data)
+	{
+		DToI dToInt;
+		return dToInt(data) % capacity;
 	}
 private:
 	vector<Node*> _table;
 	size_t _size;
 };
 
-void Test()
+void Test1()
 {
-	int arr[] = { 19,14,23,1,68,20,84,27,55,11,10,79 };
-	HashBucket<int> ht(7);
+	int arr[] = { 19,14,23,1,68,20,84,27,55,11 };
+	HashBucket<int> ht;
 
 	for (auto& e : arr)
 		ht.InsertUnique(e);
+
+	ht.Print();
+
+	ht.InsertUnique(10);
+	ht.InsertUnique(79);
+
+	ht.Print();
+
+	auto it = ht.begin();
+	while (it != ht.end())
+	{
+		cout << *it << " ";
+		it++;
+	}
+	cout << endl;
+
+	for (auto e : ht)
+		cout << e << " ";
+	cout << endl;
+}
+
+void Test2()
+{
+	HashBucket<string,Str2Int> ht;
+
+	ht.InsertEqual("apple");
+	ht.InsertEqual("orange");
+	ht.InsertEqual("banana");
 
 	ht.Print();
 }
